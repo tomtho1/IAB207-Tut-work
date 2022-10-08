63659d60d7d30e2d28f63ceabb073daf7bc532e6
Tom Thorpe <tomthorpe1234@gmail.com>
2022-10-08 16:14:06 +1000

    Revert "Initial Commit"
    This reverts commit c88e62036df185dcd88a561012dadd66b3be3086.

--- main.py
+++
@@ -1,6 +0,0 @@
-from travel import create_app
-
-if __name__ == '__main__':
-    new_app = create_app()
-    new_app.run(debug=True) #if app crashes it will be rerun with errors presented by the interpreter 
-    
